dfef735875dfec34ddd10ea30e13315152b9c483
K2335 <K2335@student.jamk.fi>
2017-01-26 10:22:26 +0200

    Car Example done

---
+++ CarApplication1/Car.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarApplication1
+{
+    class Car
+    {
+        // properties
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public double Engine { get; set; }
+        public int Speed { get; set; }
+        public bool FuzzyDices { get; set; }
+        public int DoorCount { get; set; }
+        public static int SomeProperty { get; set; }
+
+        // default constructor
+        public Car()
+        {
+
+        }
+
+        // constructor.. default
+        public Car(string model)
+        {
+            Model = model;
+        }
+
+      
+        // method to give more speed
+        public void Accelerate()
+        {
+            Speed += 10;
+        }
+
+        // method to slow down
+        public void Brake(int value)
+        {
+            Speed -= value;
+        }
+
+        // method to display car data
+        public void PrintData()
+        {
+            Console.WriteLine("Car data : ");
+            Console.WriteLine("- model : " + Model);
+            Console.WriteLine("- color : " + Color);
+            Console.WriteLine("- engine : " + Engine);
+            Console.WriteLine("- speed : " + Speed);
+            Console.WriteLine("- fuzzy dices : " + FuzzyDices);
+            Console.WriteLine("- door count : " + DoorCount);
+        }
+
+        // destructor
+        ~Car()
+        {
+            Console.WriteLine("Car object destroyed.");
+        }
+    }
+}
--- CarApplication1/Program.cs
+++ CarApplication1/Program.cs
@@ -10,6 +10,34 @@ namespace CarApplication1
     {
         static void Main(string[] args)
         {
+            // create a one car instance
+            Car car = new Car();
+            car.Model = "Seat Ibiza";
+            car.Color = "Yellow";
+            car.Engine = 1.0;
+            car.Speed = 10;
+            car.FuzzyDices = true;
+            car.DoorCount = 2;
+            Car.SomeProperty = 100;
+            // display car data
+            car.PrintData();
+            Console.ReadLine();
+            // accelerate, change car's speed value
+            car.Accelerate();
+            car.Brake(2);
+            // display car data again, notice changed speed value!
+            car.PrintData();            
+            Console.ReadLine();
+
+            // create a new instance from Car class
+            Car nascar = new CarApplication1.Car("Speedstrep");
+            nascar.PrintData();
+            nascar.Color = "Black";
+            nascar.Engine = 5.0;
+            nascar.FuzzyDices = true;
+            nascar.PrintData();
+
+
         }
     }
 }
