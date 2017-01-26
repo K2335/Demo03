a1f4cc3dddf494312c45bb0243ccaa47447ea93b
K2335 <K2335@student.jamk.fi>
2017-01-26 10:51:57 +0200

    HeaterApp done

---
+++ HeaterApplication/Heater.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeaterApplication
+{
+    class Heater
+    {
+        // properties
+        public bool IsOn { get; set; }
+        public int Temperature { get; set; }
+        public double Humidity { get; set; }
+        // constructors, ei pakollinen
+       
+        // methods
+        public void PrintDetails()
+        {
+            Console.WriteLine("Power: " + IsOn);
+            Console.WriteLine("Temperature: " + Temperature);
+            Console.WriteLine("Humidity: " + Humidity);
+        }
+
+        public void Power(bool set)
+        {
+            IsOn = true;
+        }
+
+       
+    }
+}
---
+++ HeaterApplication/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeaterApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create one instance of heater class
+
+            Heater harvia = new Heater();
+
+            // set values
+            harvia.IsOn = true;
+            harvia.Temperature = 100;
+            harvia.Humidity = 70;
+
+            // testing...
+            harvia.PrintDetails();
+        }
+    }
+}
