b4f8dc497f11ec7d29aa1e02a402be3b4f372f0e
Ville <Ville@VILLE-PC>
2017-01-27 16:30:31 +0200

    Assignment02 done

---
+++ Assignment02/Pesukone.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment02
+{
+    class Pesukone
+    {
+        // properties
+        public bool IsOn { get; set; }
+        public int Program { get; set; }
+        public bool Sling { get; set; }
+        // constructors, ei pakollinen
+
+        // methods
+        public void PrintDetails()
+        {
+            Console.WriteLine("Power: " + IsOn);
+            Console.WriteLine("Program: " + Program);
+            Console.WriteLine("Sling: " + Sling);
+        }
+
+        public void Power(bool set)
+        {
+            IsOn = false;
+            Sling = false;
+        }
+    }
+}
---
+++ Assignment02/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create one instance of heater class
+            Pesukone keijo = new Pesukone();
+            keijo.PrintDetails();
+            Console.ReadLine();
+
+            // set values
+            keijo.IsOn = true;
+            keijo.Program = 3;
+            keijo.Sling = true;
+
+            // testing...
+            keijo.PrintDetails();
+            Console.WriteLine();
+        }
+    }
+}
