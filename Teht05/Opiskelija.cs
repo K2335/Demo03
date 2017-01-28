b60902ca07b2b861fdb67742c6a61c5ed44f52e3
Ville <Ville@VILLE-PC>
2017-01-28 13:35:58 +0200

    Teht05 done

---
+++ Teht05/Opiskelija.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht05
+{
+    class Opiskelija
+    {
+        // properties
+        public string Name { get; set; }
+        public int StudentID { get; set; }
+        public int Grade { get; set; }
+
+        // constructor
+        public Opiskelija()
+        {
+            Name = "";
+            StudentID = 0;
+            Grade = 0;
+        }
+
+
+        // methods
+        public void PrintData()
+        {
+            Console.WriteLine("Name of the student: " + Name);
+            Console.WriteLine("Student-ID: " + StudentID);
+            Console.WriteLine("Grade from the programming course: " + Grade);
+        }
+    }
+}
---
+++ Teht05/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Opiskelija keijo = new Opiskelija();
+            keijo.PrintData();
+            Console.ReadLine();
+
+            keijo.Name = "Keijo Kalastaja";
+            keijo.StudentID = 4580;
+            keijo.Grade = 4;
+            keijo.PrintData();
+            Console.ReadLine();
+
+            Opiskelija erno = new Opiskelija();
+            erno.Name = "Erno Palomies";
+            erno.StudentID = 6514;
+            erno.Grade = 2;
+            erno.PrintData();
+            Console.ReadLine();           
+        }
+    }
+}
