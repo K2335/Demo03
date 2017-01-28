b5188b0e5aa4a05899f3c1c0171e1f1e2ab17f7b
Ville <Ville@VILLE-PC>
2017-01-28 13:53:24 +0200

    Teht06 done

---
+++ Teht06/Kahvinkeitin.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht06
+{
+    class Kahvinkeitin
+    {
+        // properties
+        public string Name { get; set; }
+        public int Cups { get; set; }
+        public bool Ready { get; set; }
+        public bool Power { get; set; }
+
+        // constructor
+        public Kahvinkeitin()
+        {
+            Name = "";
+            Ready = false;
+            Power = false;
+    
+        }
+
+
+        // methods
+        public void PrintData()
+        {
+            Console.WriteLine("Brand name of the coffee machine: " + Name);
+            Console.WriteLine("Coffee machine is on: " + Power);
+            Console.WriteLine("How many cups would you like?: " + Cups);
+            Console.WriteLine("Coffee is ready: " + Ready);
+        }
+    }
+}
---
+++ Teht06/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kahvinkeitin braun = new Kahvinkeitin();
+            braun.PrintData();
+            Console.ReadLine();
+
+            // set value
+            braun.Name = "Braun";
+            braun.Cups = 3;
+            braun.Ready = true;
+            braun.Power = true;
+
+            braun.PrintData();
+            Console.ReadLine();
+        }
+    }
+}
