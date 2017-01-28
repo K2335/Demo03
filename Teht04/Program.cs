fd3d99c41a25578dfa59cf425a8fbac970d1423a
Ville <Ville@VILLE-PC>
2017-01-28 13:17:30 +0200

    Teht04 done

---
+++ Teht04/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicle car = new Vehicle();
+            car.PrintData();
+            Console.ReadLine();
+
+
+            car.Name = "Seat Ibiza";
+            car.Speed = 80;
+            car.Tyres = 4;
+            car.PrintData();
+            Console.ReadLine();
+            Console.Write(car.ToString());
+
+        }
+    }
+}
---
+++ Teht04/Vehicle.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht04
+{
+    class Vehicle
+    {
+        // properties
+        public string Name { get; set; }
+        public int Speed { get; set; }
+        public int Tyres { get; set; }
+
+        // constructor
+        public Vehicle()
+        {
+            Name = "Car";
+            Speed = 0;
+            Tyres = 0;
+        }
+
+
+        // methods
+        public void PrintData()
+        {
+            Console.WriteLine("Name of the car: " + Name);
+            Console.WriteLine("Speed of the car: " + Speed);
+            Console.WriteLine("Cars tyre count: " + Tyres);
+        }
+        public override string ToString()
+        {
+            return "Name: "+ Name + "\nSpeed: " + Speed + "\nTyres: " + Tyres;
+        }
+
+
+
+    }
+}
